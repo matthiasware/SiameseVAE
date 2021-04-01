7c6c65fbe1a9d51856ddf8e5c2f76fda4aa4992d
matthiasware <matthias.mitterreiter@uni-jena.de>
2021-04-01 13:57:14 +0200

    added generic projector & BarlowTwin implementation

---
+++ models/barlow_twins.py
@@ -0,0 +1,41 @@
+import torch
+
+
+class BarlowTwins(torch.nn.Module):
+    def __init__(self, backbone, projector, loss_param_scale, loss_param_lmbda):
+        super().__init__()
+        self.backbone = backbone
+        self.projector = projector
+
+        # affine = False -> no learnable parameters
+        self.bn = torch.nn.BatchNorm1d(
+            projector[-1].out_features, affine=False)
+
+        self.loss_param_scale = loss_param_scale
+        self.loss_param_lmbda = loss_param_lmbda
+
+    def forward(self, x1, x2):
+        z1 = self.projector(self.backbone(x1))
+        z2 = self.projector(self.backbone(x2))
+
+        # emprical cross-correlation matrix
+        c = self.bn(z1).T @ self.bn(z2)
+
+        loss = self.loss(c)
+        return loss
+
+    def off_diagonal(self, x):
+        # return a flattened view of the
+        # off-diagonal elements of a square matrix
+        n, m = x.shape
+        assert n == m
+        return x.flatten()[:-1].view(n - 1, n + 1)[:, 1:].flatten()
+
+    def loss(self, c):
+        on_diag = torch.diagonal(
+            c).add_(-1).pow_(2).sum().mul(self.loss_param_scale)
+        off_diag = self.off_diagonal(c).pow_(
+            2).sum().mul(self.loss_param_scale)
+        #
+        loss = on_diag + self.loss_param_lmbda * off_diag
+        return loss
---
+++ models/projectors.py
@@ -0,0 +1,51 @@
+import torch
+
+
+def get_linear_projection_block_layers(
+        d_in,
+        d_out,
+        dropout_rate=0.1,
+        normalize=False,
+        activation=True):
+    layers = []
+    layers.append(torch.nn.Linear(d_in, d_out))
+    if normalize:
+        layers.append(torch.nn.BatchNorm1d(d_out))
+    if activation:
+        layers.append(torch.nn.ReLU(inplace=True))
+    if dropout_rate is not None:
+        layers.append(torch.nn.Dropout(p=dropout_rate))
+    return layers
+
+
+def get_projection_head_layers(
+        d_in,
+        d_out,
+        d_hidden,
+        n_hidden,
+        normalize,
+        dropout_rate,
+        activation_last,
+        normalize_last,
+        dropout_rate_last):
+
+    dims = [d_in]
+    for _ in range(n_hidden):
+        dims.append(d_hidden)
+    dims.append(d_out)
+    #
+    all_layers = []
+    for i in range(len(dims) - 2):
+        layers = get_linear_projection_block_layers(
+            dims[i], dims[i + 1], dropout_rate, normalize)
+        all_layers += layers
+
+    all_layers += get_linear_projection_block_layers(
+        dims[-2], dims[-1], dropout_rate_last, normalize_last, activation_last)
+    return all_layers
+
+
+def get_projector(**kwargs):
+    layers = get_projection_head_layers(**kwargs)
+    projector = torch.nn.Sequential(*layers)
+    return projector
